85f7fcf00084b06a6e9065677936fd2d28bdd422
myjeong19 <myjeong19@naver.com>
2024-08-19 22:10:52 +0900

    FIX. 이메일 로그인

--- src/pages/auth/login__email/EmailLogin.tsx
+++ src/pages/auth/login__email/EmailLogin.tsx
@@ -31,7 +31,7 @@ export default function EmailLogin() {
         <span>
           <img src={line} alt='line' />
         </span>
-        <Link to='/forgot'>계정찾기</Link>
+        <Link to='/account-recovery'>계정찾기</Link>
       </nav>
     </AuthContainer>
   );
